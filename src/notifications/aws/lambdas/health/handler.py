686a092e35750c871e57a228e8d232a95d5d7b92
Roman Reznikov <romanreznikov2002@yandex.ru>
2024-08-01 21:18:17 -0400

    chore: change aws lambda files structure

--- src/notifications/aws/lambdas/health/handler.py
+++ src/notifications/aws/lambdas/health/handler.py
@@ -1,7 +1,13 @@
+from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
+from .constants import HEALTH
 
-def lambda_handler(event: dict, context: LambdaContext) -> dict:
+logger = Logger()
+
+
+def health_handler(event: dict, context: LambdaContext) -> dict:
+    logger.info("Debug health %s", HEALTH)
     message = "Hello {} {}!".format(event.get("first_name", "John"), event.get("last_name", "Doe"))
     return {
         "message": message,
